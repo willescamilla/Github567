fcb047c6204576dc5ae3a42ade00d9315266170d
William Escamilla <wescamilla10@gmail.com>
2022-03-03 18:35:46 -0500

    Trying to get travic-ci and mock to work part 1

--- TestGithubAPI.py
+++ TestGithubAPI.py
@@ -5,17 +5,17 @@ from githubapi import GithubApi
 
 @mock.patch('githubapi.GithubApi')
 def mock_github_call(mock_GithubApi_func):
-    mock_GithubApi_func.return_value = []
+    mock_GithubApi_func.return_value = False
 
 class TestGithubAPI(unittest.TestCase):
     def testGithubNone(self):
-        self.assertEqual(GithubApi('?'), [])
+        self.assertEqual(GithubApi('?'), False)
     def testGithubNone2(self):
-        self.assertEqual(GithubApi('willesc'), [])
+        self.assertEqual(GithubApi('willesc'), False)
     def testGithubSelf(self):
-        self.assertEqual(len(GithubApi('willescamilla')), 25)
+        self.assertEqual(len(GithubApi('willescamilla')), True)
     def testNoId(self):
-        self.assertEqual(GithubApi(''), [])
+        self.assertEqual(GithubApi(''), False)
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
